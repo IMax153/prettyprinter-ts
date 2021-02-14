2a30de1243a01882d0aa37d950dede954f71c1a1
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-02-14 17:50:09 -0500

    Add rendering algorithms

--- src/Render.ts
+++ src/Render.ts
@@ -5,6 +5,9 @@ import { absurd, pipe } from 'fp-ts/function'
 import * as M from 'fp-ts/Monoid'
 import * as RA from 'fp-ts/ReadonlyArray'
 
+import type { Doc } from './Doc'
+import * as L from './Layout'
+import * as PW from './PageWidth'
 import type { SimpleDocStream } from './SimpleDocStream'
 import * as SDS from './SimpleDocStream'
 
@@ -13,6 +16,31 @@ import * as SDS from './SimpleDocStream'
 // -------------------------------------------------------------------------------------
 
 const foldS = M.fold(M.monoidString)
+
+/**
+ * @category rendering algorithms
+ * @since 0.0.1
+ */
+export const render = <A>(doc: Doc<A>): string =>
+  pipe(L.defaultLayoutOptions, L.layoutPretty(doc), renderS)
+
+/**
+ * @category rendering algorithms
+ * @since 0.0.1
+ */
+export const renderWidth = (lineWidth: number, ribbonFraction = 1) => <A>(doc: Doc<A>): string =>
+  pipe(
+    L.LayoutOptions(PW.AvailablePerLine(lineWidth, ribbonFraction)),
+    L.layoutPretty(doc),
+    renderS
+  )
+
+/**
+ * @category rendering algorithms
+ * @since 0.0.1
+ */
+export const renderUnbounded = <A>(doc: Doc<A>): string =>
+  pipe(L.LayoutOptions(PW.Unbounded), L.layoutPretty(doc), renderS)
 
 /**
  * @category rendering algorithms
